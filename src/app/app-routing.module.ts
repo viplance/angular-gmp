9b557f3b31a7a9ca31e78cd1768148b696c0bb41
Dzmitry Sharko <viplance.com@gmail.com>
2020-04-05 21:29:09 +0300

    [task-7] Implemented auth guard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService as AuthGuard } from './modules/shared/services';
 import { NotFoundComponent } from 'app/modules/shared/components';
 
 const routes: Routes = [
   {
     path: 'courses',
     loadChildren: () => import('./modules/courses/courses.module').then((module) => module.CoursesModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'user',
